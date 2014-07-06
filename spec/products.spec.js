5fd0d96c54748534a1a2506a7114d6993a3f8c1e
kiwi <jcwen@thoughtworks.com>
2014-07-06 23:52:06 +0800

    change it description

--- spec/products.spec.js
+++ spec/products.spec.js
@@ -7,34 +7,36 @@ var app = require("../app");
 var Product = mongoose.model('Product');
 
 describe("Product", function () {
-    var product;
+    describe("GET", function() {
+        var product;
 
-    beforeEach(function (done) {
-        mockgoose.reset();
-        product = new Product({name: 'apple juice', description: 'good', price: 10.12});
-        product.save(done);
-    });
+        beforeEach(function (done) {
+            mockgoose.reset();
+            product = new Product({name: 'apple juice', description: 'good', price: 10.12});
+            product.save(done);
+        });
 
-    it('http status 200', function (done) {
-        request(app)
-            .get('/products/' + product.id)
-            .expect(200)
-            .end(function(err, res) {
-                expect(res.body.name).toBe('apple juice')
-                expect(res.body.description).toBe('good')
-                expect(res.body.uri).toContain("/products/" + product.id)
-                done();
-            });
-    });
+        it('existing product', function (done) {
+            request(app)
+                .get('/products/' + product.id)
+                .expect(200)
+                .end(function(err, res) {
+                    expect(res.body.name).toBe('apple juice')
+                    expect(res.body.description).toBe('good')
+                    expect(res.body.uri).toContain("/products/" + product.id)
+                    done();
+                });
+        });
 
-    it('http status 404', function (done) {
-        request(app)
-            .get('/products/not_exist_product_id')
-            .expect(404, done);
-    });
+        it('non-existing product', function (done) {
+            request(app)
+                .get('/products/not_exist_product_id')
+                .expect(404, done);
+        });
 
-    afterEach(function (done) {
-        mockgoose.reset();
-        done();
+        afterEach(function (done) {
+            mockgoose.reset();
+            done();
+        });
     });
 });
