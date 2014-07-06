927a51ce9a600f16ae43fde192cb4b7cbb6f551e
kiwi <jcwen@thoughtworks.com>
2014-07-07 01:52:34 +0800

    add ger order by id

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,12 @@ var Order = mongoose.model('Order');
 var Product = mongoose.model('Product');
 
 router.get('/:userId/orders/:orderId', function(req, res) {
-  res.send('respond with a resource');
+	return Order.findById(req.params.orderId)
+				.populate('product')
+				.populate('user')
+				.exec(function (err, order) {
+					return res.send(200, {id: order.id, price: order.product.price})
+				})
 });
 
 module.exports = router;
--- spec/orders.spec.js
+++ spec/orders.spec.js
@@ -17,7 +17,7 @@ describe('Order', function() {
 		beforeEach(function (done) {
 	        mockgoose.reset();
 	        product = new Product({name: 'apple juice', description: 'good', price: 10.12});
-	        product.save(done);
+	        product.save();
 
 			user = new User({name: "kiwi"})
 			user.save(function (err) {
@@ -25,6 +25,8 @@ describe('Order', function() {
 					product: product._id,
 					user: user._id
 				});
+
+				order.save(done);
 			});
 		});
 
@@ -36,7 +38,12 @@ describe('Order', function() {
 		it('exist order', function (done) {
 			request(app)
 				.get("/users/" + user.id + "/orders/" + order.id)
-				.expect(200, done);
+				.expect(200)
+				.end(function (err, res) {
+					expect(res.body.price).toBe(10.12);
+
+					done();
+				});
 		})
 	})
 })
